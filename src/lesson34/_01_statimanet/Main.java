c886571251c2c6ba91d45b8193ef13291e305160
DUPLETION <7273391s@gmail.com>
2024-06-17 10:59:24 +0300

    g

--- src/lesson34/_01_statimanet/Main.java
+++ src/lesson34/_01_statimanet/Main.java
@@ -7,9 +7,12 @@ import java.sql.DriverManager;
 public class Main {
     @SneakyThrows
     public static void main(String[] args) {
-        Class.forName("com.mysql.cj.jsbc.Driver");
+        Class.forName("com.mysql.cj.jdbc.Driver");
 
-        try (var cn = DriverManager.getConnection("jdbc:mysql://localhost:3306")){
+        try (var cn = DriverManager.getConnection("jdbc:mysql://localhost:3306/po_2307", "root", "");
+        var st = cn.createStatement()){
+            var query = "INSERT INTO staff(name, age, department, project) VALUES ('employee11', 27, 5, 3)";
+            st.execute(query);//возвращает boolean, execute boolean - возвращает Int кол-во строк которые обновил
 
         }
     }
---
+++ src/lesson34/_02_execute_query/Staff.java
@@ -0,0 +1,13 @@
+package lesson34._02_execute_query;
+
+
+import lombok.AllArgsConstructor;
+import lombok.ToString;
+
+@AllArgsConstructor
+@ToString
+public class Staff {
+    private String name;
+    private int age;
+    private String department;
+}
